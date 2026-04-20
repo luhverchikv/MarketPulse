898eeb0e99c98c6a7ba2169544e7f14fa20b2f91
luhverchikv <luhverchikv@icloud.com>
2026-04-20 13:42:50 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,34 +1,18 @@
 # main.py
 import asyncio
 from aiogram import Bot, Dispatcher
-#from aiogram.client.default import DefaultBotProperties
-#from aiogram.enums import ParseMode
-
 from config import config
 #from db import init_db
+
 from menu import router as menu_router
-#from scheduler import scheduler_worker
-
-
-
-
-#async def on_startup(bot: Bot):
-    #"""Выполняется при запуске бота"""
-    #init_db()
-    #print(f"✅ {config.app.project_name} запущен | БД: {DB_FULL_PATH}")
-    
-    # Запускаем планировщик в фоне
-    #if config.scheduler.enabled:
-        #asyncio.create_task(
-            #scheduler_worker(bot, config.scheduler.daily_digest_time)
-        #)
+from api.youtube import router as youtube_router
 
 
 async def main():
     bot = Bot(token=config.bot.token)
     dp = Dispatcher()
     dp.include_router(menu_router)
-    #dp.startup.register(on_startup)
+    dp.include_router(youtube_router)
     
     # Запуск поллинга
     await dp.start_polling(bot)
